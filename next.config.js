82c2631f027e06218102719d9759e916be020673
Guray Cha <guraycha@gmail.com>
2025-05-14 21:56:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,18 +3,11 @@
 
 // next.config.js
 
-const repoName = 'uni-connect-forum'; // <-- CONFIRM THIS MATCHES YOUR REPO
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... other configs
-  //output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '',
   images: {
-    unoptimized: true,
+    unoptimized: true, // Optional: Only if you're not using optimized images
   },
-  // ...
 };
 
 module.exports = nextConfig;
